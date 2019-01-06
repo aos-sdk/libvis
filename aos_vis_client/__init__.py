ce85226084d07f4d2e255803f31659fb3d357f40
Oleh Mykytyuk <oleh.mykytyuk@gmail.com>
2019-01-06 20:25:22 +0200

    Fixed imports
    Enable import of needed modules in simple way
     which used in demo apps:
     from aos_vis_client import VISClient
    
    Signed-off-by: Oleh Mykytyuk <oleh.mykytyuk@gmail.com>

--- aos_vis_client/__init__.py
+++ aos_vis_client/__init__.py
@@ -11,3 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from aos_vis_client.vis_client import VISClient
+from aos_vis_client.vis_data_accessors.vis_subscription import VISDataAccessor, VISDataSubscription
